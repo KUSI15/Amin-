32010a61959c8cabe1121e14ec3175ac61ab1526
KUSI15 <132665334+KUSI15@users.noreply.github.com>
2024-10-30 06:53:28 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+console.log('Hello World!');
+alert("Hello World!")
+alert("Warning!")
+prompt("meron ba?")
+alert("uhmm")
+alert("maysasabihin ako Heheheh")
+alert("uhmmm")
+alert("G")
+alert("U")
+alert("S")
+alert("T")
+alert("O")
+alert("K")
+alert("I")
+alert("T")
+alert("A")
+prompt("HUHUHUUHUHUHUHU")
+alert(":(")
+
+const result = confirm("TULOY MOPA BAA?");
+if (result) {
+  // User clicked "OK"
+  console.log("User accepted");
+  alert("okii")
+} else {
+  // User clicked "Cancel"
+  window.open('','_self').close(); // Exit the site
+  alert("sana ok parin tayo asalya.")
+}
